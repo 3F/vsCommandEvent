45b39173002294a5ecbb40d3192c130ddb463fe1
Denis Kuzmin (github/3F) <x-3F@outlook.com>
2022-02-12 18:59:46 +0300

    vssbe build up to 1.15

--- build.bat
+++ build.bat
@@ -4,7 +4,6 @@ REM https://github.com/3F/vsSolutionBuildEvent/pull/45#issuecomment-506754001
 set hMSBuild=-notamd64
 
 set cim=packages\vsSolutionBuildEvent\cim.cmd
-set _gnt=tools\gnt
 
 REM # Verbosity level: q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic].
 set level=m
@@ -13,9 +12,7 @@ REM # Configuration name without postfix _SDK...
 :: DBG == Debug; REL == Release; DCI == CI Debug; RCI == CI Release;
 
 set reltype=%~1
-if "%reltype%"=="" (
-    set "reltype=DCI"
-)
+if "%reltype%"=="" set "reltype=DCI"
 
 :::: --------
 
@@ -23,7 +20,7 @@ set __p_call=1
 
 :: Activate vsSBE
 
-call %_gnt% /p:ngpath="%cd%/packages" /p:ngconfig="%cd%/.gnt/packages.config" || goto err
+call tools\gnt /p:ngpath="%cd%/packages" /p:ngconfig="%cd%/tools/packages.config" || goto err
 
 :: Build
 
@@ -37,7 +34,7 @@ goto ok
 
 :err
 
-echo. Build failed. 1>&2
+echo. Failed. 1>&2
 exit /B 1
 
 :ok
