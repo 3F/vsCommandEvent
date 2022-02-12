786d5fd460af6cfb54332a04589bfd122cbd1e24
Denis Kuzmin (github/3F) <x-3F@outlook.com>
2022-02-12 20:47:44 +0300

    sync at 7a40c50fa24f303d3c909b96cea42d4382a4fdbf

--- vsCommandEvent/Environment.cs
+++ vsCommandEvent/Environment.cs
@@ -37,13 +37,12 @@ namespace net.r_eg.vsCE
 
         private string startupProject;
 
+        private ConfigItem currentSlnConf;
+
         /// <summary>
         /// List of EnvDTE projects.
         /// </summary>
-        public IEnumerable<DProject> ProjectsDTE
-        {
-            get => _DTEProjects;
-        }
+        public IEnumerable<DProject> ProjectsDTE => _DTEProjects;
 
         /// <summary>
         /// List of Microsoft.Build.Evaluation projects.
@@ -268,6 +267,24 @@ namespace net.r_eg.vsCE
                 }
             }
         }
+
+        protected override ConfigItem ActiveSlnConf
+        {
+            get
+            {
+                SolutionConfiguration2 cfg = SolutionActiveCfg;
+                if(cfg == null) return null;
+
+                if(currentSlnConf?.IsEqualByRule(cfg.Name, cfg.PlatformName) == true)
+                {
+                    return currentSlnConf;
+                }
+
+                currentSlnConf = new(cfg.Name, cfg.PlatformName);
+                return currentSlnConf;
+            }
+        }
+
 
         /// <summary>
         /// An unified unscoped and out of Project instance the property value by its name.
--- vsCommandEvent/IsolatedEnv.cs
+++ vsCommandEvent/IsolatedEnv.cs
@@ -28,6 +28,8 @@ namespace net.r_eg.vsCE
 
         protected readonly IConfPlatform defaultCfg = new ConfigSln("Debug", "Any CPU");
 
+        private ConfigItem currentSlnConf;
+
         private string _startupProjectString;
 
         private readonly IDictionary<string, string> _properties;
@@ -164,6 +166,8 @@ namespace net.r_eg.vsCE
             get => __disabled<IOW>(nameof(OutputWindowPane));
         }
 
+        protected override ConfigItem ActiveSlnConf => extractCfg(slnProperties);
+
         /// <summary>
         /// An unified unscoped and out of Project instance the property value by its name.
         /// Remarks: Any property values cannot be null.
@@ -290,14 +294,20 @@ namespace net.r_eg.vsCE
             return properties;
         }
 
-        protected IConfPlatform extractCfg(IDictionary<string, string> properties)
+        protected ConfigItem extractCfg(IDictionary<string, string> properties)
         {
             IConfPlatform def = Sln?.DefaultConfig;
 
-            return new ConfigItem(
-                properties.GetOrDefault(PropertyNames.CONFIG, def?.Configuration),
-                properties.GetOrDefault(PropertyNames.PLATFORM, def?.Platform)
-            );
+            string configuration = properties.GetOrDefault(PropertyNames.CONFIG, def?.Configuration);
+            string platform = properties.GetOrDefault(PropertyNames.PLATFORM, def?.Platform);
+
+            if(currentSlnConf?.IsEqualByRule(configuration, platform) == true)
+            {
+                return currentSlnConf;
+            }
+
+            currentSlnConf = new(configuration, platform);
+            return currentSlnConf;
         }
 
         protected string formatCfg(IDictionary<string, string> properties)
